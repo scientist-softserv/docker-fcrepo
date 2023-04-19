781ac091a3d83f080b392411d3f262e534d75e4c
braydonjustice <braydonjustice@gmail.com>
2023-04-19 12:01:08 -0700

    update repack script

--- assets/repack.sh
+++ assets/repack.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 echo "Downloading Fedora v${FCREPO_VERSION} .war file"
-curl -# -Lo /build/fedora.war https://github.com/fcrepo4/fcrepo4/releases/download/fcrepo-${FCREPO_VERSION}/fcrepo-webapp-${FCREPO_VERSION}.war
+curl -# -Lo /build/fedora.war https://github.com/scientist-softserv/fcrepo/releases/download/fcrepo-${FCREPO_VERSION}/fcrepo-webapp-${FCREPO_VERSION}.war
 echo "Extracting Fedora v${FCREPO_VERSION} .war file"
 jar -xf /build/fedora.war
 
-curl --silent -I https://raw.githubusercontent.com/fcrepo4/fcrepo4/fcrepo-${FCREPO_VERSION}/fcrepo-webapp/src/main/jetty-console/WEB-INF/web.xml \
+curl --silent -I https://raw.githubusercontent.com/scientist-softserv/fcrepo/fcrepo-${FCREPO_VERSION}/fcrepo-webapp/src/main/jetty-console/WEB-INF/web.xml \
   | head -1 \
   | grep "200 OK" > /dev/null
 
 if [[ $? == 0 ]]; then
   echo "Downloading Fedora v${FCREPO_VERSION} one-click web.xml"
-  curl -# -Lo WEB-INF/web.xml https://raw.githubusercontent.com/fcrepo4/fcrepo4/fcrepo-${FCREPO_VERSION}/fcrepo-webapp/src/main/jetty-console/WEB-INF/web.xml
+  curl -# -Lo WEB-INF/web.xml https://raw.githubusercontent.com/scientist-softserv/fcrepo/fcrepo-${FCREPO_VERSION}/fcrepo-webapp/src/main/jetty-console/WEB-INF/web.xml
   echo "Repacking Fedora v${FCREPO_VERSION} .war file"
   jar -cf /build/fedora.war .
 fi
